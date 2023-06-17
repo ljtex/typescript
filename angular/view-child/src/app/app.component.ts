7b990b0f7fa5c5c686d8ce520c2e7218fc2fa9f0
Lijun Jiang <jiang201678@gmail.com>
2023-06-16 19:56:24 -0500

    ViewChild for child component not working?

--- angular/view-child/src/app/app.component.ts
+++ angular/view-child/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'view-child';
+
+  @ViewChild('dobInput')
+  dateOfBirth: ElementRef = undefined as any;
+
+  @ViewChild('ageInput')
+  age: ElementRef = undefined as any;
+
+  @ViewChild(DemoComponent, {static: true } ) demoComp: DemoComponent;
+
+  calcAge() {
+
+    let birthYear = new Date(this.dateOfBirth.nativeElement.value).getFullYear();
+    this.age.nativeElement.value = new Date().getFullYear() - birthYear;
+  }
 }
